21f79480d4606020f94e2a45784c9d6c6afb93be
Samantha Wette <samantha.wette@gmail.com>
2022-04-13 12:03:38 -0700

    madlibs 2

--- madlibs.py
+++ madlibs.py
@@ -8,29 +8,12 @@ from flask import Flask, render_template, request
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
-AWESOMENESS = [
-    "awesome",
-    "terrific",
-    "fantastic",
-    "neato",
-    "fantabulous",
-    "wowza",
-    "oh-so-not-meh",
-    "brilliant",
-    "ducky",
-    "coolio",
-    "incredible",
-    "wonderful",
-    "smashing",
-    "lovely",
-]
-
 
 @app.route("/")
 def start_here():
     """Display homepage."""
 
-    return "Hi! This is the home page."
+    return render_template("home.html")
 
 
 @app.route("/hello")
@@ -39,18 +22,66 @@ def say_hello():
 
     return render_template("hello.html")
 
-
 @app.route("/greet")
 def greet_person():
-    """Greet user with compliment."""
+    # """Ask user if they would like to play."""
 
-    player = request.args.get("person")
+    return render_template("compliment.html")
 
-    compliment = choice(AWESOMENESS)
+@app.route("/game")
+def show_madlib_form():
+    """Asks the user if they want to play a game"""
+    
+    #render_template("compliment.html")
+    game_decision = request.args.get("game")
+    print(game_decision)
+    if game_decision =='no': 
+        return render_template("goodbye.html")
+    else:   
+        return render_template("game.html")
+    
 
-    return render_template("compliment.html", person=player, compliment=compliment)
+@app.route("/madlib")
+def show_madlib():
+    """Return the final madlib."""
+
+    number = request.args.get("number")
+    verb = request.args.get("verb")
+    adverb = request.args.get("adverb")
+    feeling = request.args.get("feeling")
+    adjective = request.args.get("adjective")
+    noun = request.args.get("noun")
+    another_number = request.args.get("another-number")
+    another_adjective = request.args.get("another-adjective")
+    third_adjective = request.args.get("third-adjective")
+    instructor_name = request.args.get("instructor-name")
+    animal = request.args.get("animal")
+    niche_topic = request.args.get("niche-topic")
+    another_verb = request.args.get("another-verb")
+    time_of_day = request.args.get("time-of-day")
+    last_number = request.args.get("last-number")
+    name = request.args.get("name")
 
 
+    return render_template("madlib.html", 
+                            number=number,
+                            verb=verb,
+                            adverb=adverb,
+                            feeling=feeling,
+                            adjective=adjective,
+                            noun=noun,
+                            another_number=another_number,
+                            another_adjective=another_adjective,
+                            third_adjective =third_adjective,
+                            instructor_name=instructor_name,
+                            animal = animal,
+                            niche_topic = niche_topic,
+                            another_verb = another_verb,
+                            time_of_day = time_of_day,
+                            last_number = last_number,
+                            name=name
+                            )
+    
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
